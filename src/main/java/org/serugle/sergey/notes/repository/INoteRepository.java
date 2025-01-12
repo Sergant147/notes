737fb7794a9d89b02fb7063f2b4b3fc281c09977
duhov <portoslunt@gmail.com>
2025-01-12 11:33:45 +0300

    draft

--- src/main/java/org/serugle/sergey/notes/repository/INoteRepository.java
+++ src/main/java/org/serugle/sergey/notes/repository/INoteRepository.java
@@ -1,12 +1,14 @@
 package org.serugle.sergey.notes.repository;
 
+import org.serugle.sergey.notes.model.Note;
+
 import java.util.ArrayList;
 
 
 public interface INoteRepository {
     public ArrayList<String> getNotesByUsername(String username);
 
-    public void fetchAll();
+    public ArrayList<Note> fetchAll();
 
     public String getOwnerOf(String note);
 
